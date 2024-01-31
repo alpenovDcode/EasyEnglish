751e8bfd5391020fef88e05c7010a2fbd92c9788
alpenovDcode <alpenovevgeniy05@gmail.com>
2024-01-31 20:56:39 +0600

    Initial Commit

---
+++ EasyEnglish/EasyEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyEnglishApp.swift
+//  EasyEnglish
+//
+//  Created by Альпеша on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct EasyEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
